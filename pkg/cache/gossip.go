7e6baf60fc6070b0e2db676604e5e75ff098ba51
Kevin <kevisos123@gmail.com>
2025-10-11 19:27:11 -0500

    Add gossip

---
+++ pkg/cache/gossip.go
@@ -0,0 +1,70 @@
+package cache
+
+import (
+	"github.com/hashicorp/memberlist"
+	"github.com/vmihailenco/msgpack/v5"
+)
+
+type gossipHandler struct {
+	cache      *Cache
+	broadcasts *memberlist.TransmitLimitedQueue
+}
+
+func newGossipHandler(c *Cache, ml *memberlist.Memberlist, retransmitMult int) *gossipHandler {
+	return &gossipHandler{
+		cache: c,
+		broadcasts: &memberlist.TransmitLimitedQueue{
+			NumNodes:       func() int { return ml.NumMembers() },
+			RetransmitMult: retransmitMult,
+		},
+	}
+}
+
+func (g *gossipHandler) broadcast(msg GossipMessage) {
+	buf, err := msgpack.Marshal(&msg)
+	if err != nil {
+		return
+	}
+	g.broadcasts.QueueBroadcast(&broadcast{msg: buf})
+}
+
+// Memberlist Delegate methods
+func (g *gossipHandler) NodeMeta(limit int) []byte {
+	return nil
+}
+
+func (g *gossipHandler) NotifyMsg(b []byte) {
+	var msg GossipMessage
+	if err := msgpack.Unmarshal(b, &msg); err != nil {
+		return
+	}
+	g.cache.applyGossip(msg)
+}
+
+func (g *gossipHandler) GetBroadcasts(overhead, limit int) [][]byte {
+	return g.broadcasts.GetBroadcasts(overhead, limit)
+}
+
+func (g *gossipHandler) LocalState(join bool) []byte {
+	snap := g.cache.storage.snapshot(false)
+	buf, _ := msgpack.Marshal(&snap)
+	return buf
+}
+
+func (g *gossipHandler) MergeRemoteState(buf []byte, join bool) {
+	var remote map[string]*Entry
+	if err := msgpack.Unmarshal(buf, &remote); err != nil {
+		return
+	}
+
+	for k, e := range remote {
+		g.cache.applyGossip(GossipMessage{
+			Key:       k,
+			Value:     e.Value,
+			Version:   e.Version,
+			ExpireAt:  e.ExpireAt,
+			Tombstone: e.Tombstone,
+			OriginID:  g.cache.opts.OriginID,
+		})
+	}
+}
