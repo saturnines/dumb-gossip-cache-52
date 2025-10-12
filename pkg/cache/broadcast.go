ba88174c3b9c56e52270074722518f0c1a41d272
Kevin <kevisos123@gmail.com>
2025-10-11 19:26:17 -0500

    Add broadcast wrapper for memberlist gossip queue

---
+++ pkg/cache/broadcast.go
@@ -0,0 +1,17 @@
+package cache
+
+import "github.com/hashicorp/memberlist"
+
+type broadcast struct {
+	msg []byte
+}
+
+func (b *broadcast) Invalidates(memberlist.Broadcast) bool {
+	return false
+}
+
+func (b *broadcast) Message() []byte {
+	return b.msg
+}
+
+func (b *broadcast) Finished() {}
